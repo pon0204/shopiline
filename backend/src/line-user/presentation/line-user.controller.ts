97e8b07ed0dc9b9599c8de843a55906d95f7f2d2
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-07-24 15:43:54 +0900

    feat: エンドポイントにclientIdを追加

--- backend/src/line-user/presentation/line-user.controller.ts
+++ backend/src/line-user/presentation/line-user.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateLineUserRequestDto } from './create-line-user-request.dto';
 import { CreateLineUserUsecase } from '../usecase/create-line-user.usecase';
+import { ClientValidationPipe } from 'src/common/pipes/client-validation.pipe';
 
-@Controller('line-user')
+@Controller('client/:clientId/line-user')
 export class LineUserController {
   constructor(private createLineUserUsecase: CreateLineUserUsecase) {}
 
-  @Post()
-  async createLineUser(@Body() dto: CreateLineUserRequestDto) {
-    return await this.createLineUserUsecase.handle('lineId');
+  @Get()
+  async createLineUser(
+    @Body()
+    dto: CreateLineUserRequestDto,
+    @Param('clientId', ClientValidationPipe) clientId: number,
+  ) {
+    return await this.createLineUserUsecase.handle('lineId', clientId);
   }
 }
